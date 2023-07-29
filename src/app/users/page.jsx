707c8ff4c795c4ee2ef48115609ed6f7a829deb7
hendysetiawan <hendysetiawan@inovasi360.com>
2023-07-29 12:05:50 +0700

    users data pagination

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -3,17 +3,25 @@ import React from "react";
 import { useState, useEffect } from "react";
 import bannerImage from "@/assets/images/banner_image.png";
 import { Container, Table, Col } from "react-bootstrap";
+import ReactPaginate from "react-paginate";
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import { getUsers } from "@/services/api";
 
 function page() {
   const [users, setUsers] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const handlePageClick = (event) => {
+    console.log(event.selected);
+    setPage(event.selected + 1);
+  };
   const handleGetUsers = async (page, perPage) => {
     const users = await getUsers(page, perPage);
     setUsers(users);
   };
   useEffect(() => {
-    handleGetUsers();
-  }, []);
+    handleGetUsers(page, 10);
+  }, [page]);
   return (
     <div className="bg-light-blue">
       <div
@@ -63,6 +71,26 @@ function page() {
               })}
             </tbody>
           </Table>
+          <ReactPaginate
+            activeClassName={"item active "}
+            breakClassName={"item break-me "}
+            breakLabel={"..."}
+            containerClassName={"pagination"}
+            disabledClassName={"disabled-page"}
+            marginPagesDisplayed={2}
+            nextClassName={"item next "}
+            nextLabel={
+              <AiOutlineArrowRight style={{ fontSize: 18, width: 150 }} />
+            }
+            onPageChange={handlePageClick}
+            pageCount={10}
+            pageClassName={"item pagination-page "}
+            pageRangeDisplayed={2}
+            previousClassName={"item previous"}
+            previousLabel={
+              <AiOutlineArrowLeft style={{ fontSize: 18, width: 150 }} />
+            }
+          />
         </Col>
       </Container>
     </div>
